bb446aeaf30aa4bde9b23e239098190dad79119b
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2022-10-26 13:52:10 +0300

    refactoring app

---
+++ server/src/App.ts
@@ -0,0 +1,45 @@
+import { inject, injectable } from 'inversify';
+import { IConfigService } from './services/config';
+import { IPrismaService } from './services/prisma';
+import { injectKeys } from './types/injectKeys';
+import { Context, Scenes, Telegraf } from 'telegraf';
+
+export interface MySessionScene extends Scenes.SceneSessionData {
+  sessionSceneProps: string;
+}
+
+export interface MySession extends Scenes.SceneSession<MySessionScene> {
+  sessionProps: string;
+}
+
+export interface MyContext extends Context {
+  contextProps: string;
+  session: MySession;
+  scene: Scenes.SceneContextScene<MyContext, MySessionScene>;
+}
+
+@injectable()
+export class App {
+  bot: Telegraf<MyContext>;
+
+  constructor(
+    @inject(injectKeys.IPrismaService) private prisma: IPrismaService,
+    @inject(injectKeys.IConfigService) private config: IConfigService,
+  ) {
+    this.bot = new Telegraf<MyContext>(this.config.get('BOT_TOKEN'));
+  }
+
+  async start(ctx: MyContext): Promise<void> {
+    await ctx.replyWithMarkdown('Form', {
+      reply_markup: {
+        keyboard: [[{ text: 'Заполнить форму доставки' }]],
+      },
+    });
+  }
+
+  async init(): Promise<void> {
+    await this.prisma.connect();
+    this.bot.launch();
+    this.bot.command('start', this.start);
+  }
+}
